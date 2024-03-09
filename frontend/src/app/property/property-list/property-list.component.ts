01c55279635f0e44fca29584f378659e3a8a7b7c
jerome <zachariah11jerobert@gmail.com>
2024-03-09 17:36:29 +0530

    Intoduce pipes for HttpGet to Define proper types

--- frontend/src/app/property/property-list/property-list.component.ts
+++ frontend/src/app/property/property-list/property-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { error } from 'console';
 import { HousingService } from 'src/app/services/housing.service';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-property-list',
@@ -9,7 +10,7 @@ import { HousingService } from 'src/app/services/housing.service';
 })
 export class PropertyListComponent implements OnInit {
 
-  properties: any;
+  properties: Array<IProperty>=[];
 
   constructor(private housingService: HousingService) { }
 
@@ -20,6 +21,7 @@ export class PropertyListComponent implements OnInit {
         this.properties = data;
         console.log(data);
       } , error => {
+        console.log('httperror');
         console.log(error);
       }
     );
